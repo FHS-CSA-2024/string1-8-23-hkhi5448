6c26c142194939d4e632840e0464072abb5db4c2
hkhi5448 <hk07.khinda@gmail.com>
2024-09-15 22:14:54 -0700

    String1.java
    Learned how to use strings. It was quite difficult to understand at first but after getting the basics down by reviewing youtube videos on strings, it began to get easier.

--- src/main/java/String1.java
+++ src/main/java/String1.java
@@ -39,7 +39,7 @@ public class String1
      *  helloName("X") → "Hello X!"
      */
     public String helloName(String name) {
-        return unimplemented;
+        return "Hello " + name + "!";
     }
 
     /*
@@ -50,7 +50,7 @@ public class String1
      * makeAbba("What", "Up") → "WhatUpUpWhat"
      */
     public String makeAbba(String a, String b) {
-        return unimplemented;
+        return a + b + b + a;
     }
 
     /*
@@ -62,7 +62,7 @@ public class String1
      * makeTags("cite", "Yay") → "<cite>Yay</cite>"
      */
     public String makeTags(String tag, String word) {
-        return unimplemented;
+        return "<" + tag +">" + word + "</" + tag + ">";
     }
 
     /*
@@ -76,7 +76,7 @@ public class String1
      * makeOutWord("[[]]", "word") → "[[word]]"
      */
     public String makeOutWord(String out, String word) {
-        return unimplemented;
+        return out.substring(0,2) + word + out.substring(2);
     }
 
     /*
@@ -87,7 +87,8 @@ public class String1
      * extraEnd("Hi") → "HiHiHi"
      */
     public String extraEnd(String str) {
-        return unimplemented;
+        String lastTc = str.substring(str.length() - 2);
+        return lastTc + lastTc + lastTc;
     }
 
     /*
@@ -100,7 +101,12 @@ public class String1
      * firstTwo("ab") → "ab"
      */
     public String firstTwo(String str) {
-        return unimplemented;
+        if(str.length()<2){
+            return str;
+        }
+            else{
+        return str.substring(0,2);
+        }
     }
 
     /*
@@ -110,7 +116,8 @@ public class String1
      * firstHalf("abcdef") → "abc"
      */
     public String firstHalf(String str) {
-        return unimplemented;
+        int mid = str.length()/2;
+        return str.substring(0,mid);
     }
 
     /*
@@ -121,7 +128,7 @@ public class String1
      * withoutEnd("coding") → "odin"
      */
     public String withoutEnd(String str) {
-        return unimplemented;
+        return str.substring(1, str.length()-1);
     }
 
     /*
@@ -133,7 +140,12 @@ public class String1
      * comboString("aaa", "b") → "baaab"
      */
     public String comboString(String a, String b) {
-        return unimplemented;
+        if (a.length()<b.length()){
+            return a + b + a;
+        }
+        else {
+            return b + a + b;
+        }
     }
 
     /*
@@ -144,7 +156,8 @@ public class String1
      * middleThree("solving") → "lvi"
      */
     public String middleThree(String str) {
-        return unimplemented;
+        int mid = str.length() / 2;
+        return str.substring(mid - 1,mid + 2);
     }
 
     /*
@@ -155,7 +168,14 @@ public class String1
      * extraFront("H") → "HHH"
      */
     public String extraFront(String str) {
-        return unimplemented;
+        String front;
+         if (str.length() < 2) {
+        front = str;
+        }
+        else {
+        front = str.substring(0,2);
+        }
+        return front + front + front;
     }
 
     /*
@@ -166,7 +186,7 @@ public class String1
      * left2("Hi") → "Hi"
      */
     public String left2(String str) {
-        return unimplemented;
+        return str.substring(2) + str.substring(0,2);
     }
 
     /*
@@ -178,25 +198,36 @@ public class String1
      * hasBad("xxbadxx") → false
      */
     public boolean hasBad(String str) {
+        if (str.length() >= 3 && str.substring(0,3).equals("bad")){
+            return true;
+        }
+        if (str.length() >= 4 && str.substring(1,4).equals("bad")){
+            return true;
+        }
         return false;
     }
 
     /*
      * Given two strings, append them together (known as "concatenation") and return the result. 
      * However, if the concatenation creates a double-char, then omit one of the chars, 
-     * 	so "abc" and "cat" yields "abcat".
+     *     so "abc" and "cat" yields "abcat".
      * conCat("abc", "cat") → "abcat"
      * conCat("dog", "cat") → "dogcat"
      * conCat("abc", "") → "abc"
      */
     public String conCat(String a, String b) {
-        return unimplemented;
+        if (a.length() > 0 && b.length() > 0 && a.charAt(a.length()-1) == b.charAt(0)){
+        return a + b.substring(1);
+        } 
+        else {
+        return a + b;
+        }
     }
 
     /*
      *Given two strings, append them together (known as "concatenation") and return the result. 
      *However, if the strings are different lengths, omit chars from the longer string 
-     *	so it is the same length as the shorter string. 
+     *    so it is the same length as the shorter string. 
      *So "Hello" and "Hi" yield "loHi". 
      *The strings may be any length.
      *minCat("Hello", "Hi") → "loHi"
@@ -204,7 +235,8 @@ public class String1
      *minCat("java", "Hello") → "javaello"
      */
     public String minCat(String a, String b) {
-        return unimplemented;
+        int mLength = Math.min(a.length(), b.length());
+        return a.substring(a.length() - mLength) + b.substring(b.length() - mLength);
     }
 
     /*
@@ -215,7 +247,16 @@ public class String1
      * withoutX("Hxix") → "Hxi"
      */
     public String withoutX(String str) {
-        return unimplemented;
+        if (str.length() == 0) {
+            return str;
+        }
+        if (str.charAt(0) == 'x') {
+            str = str.substring(1);
+        }
+        if (str.length() > 0 && str.charAt(str.length() - 1) == 'x') {
+            str = str.substring(0, str.length() - 1);
+        }
+        return str;
     }
 
     /*
@@ -227,8 +268,22 @@ public class String1
      * deFront("java") → "va"
      * deFront("away") → "aay"
      */
-    public String deFront(String str) {    
-        return unimplemented;
+    public String deFront(String str) { 
+        String result = "";
+        if (str.length() > 0) {
+            if (str.charAt(0) == 'a') {
+                result = result + 'a';
+            }
+        }
+        if (str.length() > 1) {
+            if (str.charAt(1) == 'b') {
+                result = result + 'b';
+            }
+        }
+        if (str.length() > 2) {
+            result += str.substring(2);
+        }
+        return result;
     }
 
 }
